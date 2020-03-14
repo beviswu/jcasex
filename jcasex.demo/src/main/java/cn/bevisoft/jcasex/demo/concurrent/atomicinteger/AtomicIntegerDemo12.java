416219e1dd5735cedb2651d8e318f682af2367b0
xubo.wuxb <xubo.wuxb@alibaba-inc.com>
2020-03-14 21:11:54 +0800

    AtomitcIntegerDemo add description

--- jcasex.demo/src/main/java/cn/bevisoft/jcasex/demo/concurrent/atomicinteger/AtomicIntegerDemo12.java
+++ jcasex.demo/src/main/java/cn/bevisoft/jcasex/demo/concurrent/atomicinteger/AtomicIntegerDemo12.java
@@ -3,11 +3,13 @@ package cn.bevisoft.jcasex.demo.concurrent.atomicinteger;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * Demo introduction: AtomicInteger累加操作 正确示例。
  * Demo description:
  * 通过20个线程并行针对一个AtomicInteger型totalCount变量累加，每个线程累加1000次，执行完之后理想的情况上20*1000=20000,
  * 把totalcount声明为AtomicInteger变量，在并发20个线程针对totalCount累加2万次之后，大部分情况还是totalCount==20000。
  * Demo notes:
  * 1.AtomicInteger能保障+1操作的原子性。
+ * 2.AtomicInteger内部实现原理也是用cas原理(循环重试)+用volatile来保障线程间的可见性。
  *
  * @author bobo.wu
  * @version : AtomicIntegerDemo12.java, v 0.1 2020年03月14日 21:00 bobo.wu Exp $
